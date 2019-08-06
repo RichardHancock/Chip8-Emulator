72b30407614c5158653fd1ec279220e2a78faa26
Richard Hancock <me@richardhancock.co.uk>
2019-08-06 22:17:17 +0100

    Laid out the switch statement for all opcodes

--- Chip8 Emulator/Chip8.cpp
+++ Chip8 Emulator/Chip8.cpp
@@ -78,27 +78,176 @@ void Chip8::emulateCycle()
 	//Compare first 4 bits
 	switch (opcode & 0xF000)
 	{
+	//0x0
 	case 0x0000: //First 4 bits not enough so need to compare last 12 bits
 		switch (opcode & 0x0FFF)
 		{
-		case 0x00E0: // 0x00E0 - Clear Screen
+		case 0x00E0: // 00E0 - Clear Screen
 
 			break;
-		case 0x00EE:
+		case 0x00EE: // 00EE - Return from Subroutine
 
 			break;
 
-		default: //0x0NNN - Calls RCA 1802 program at address NNN. Not necessary for most ROMs. (PROBABLY Won't be in any ROMS)
-			Log::logW("Unimplemented opcode: " + convertOpcodeToPrintableHex(opcode));
+		default: //0x0NNN - Calls RCA 1802 program at address NNN. Not necessary for emulators according to a few sources.
+			Log::logW("Unimplemented or Unknown opcode: " + convertOpcodeToPrintableHex(opcode));
 			break;
 		}
 		break;
 
+	//0x1
+	case 0x1000: //1NNN - Jump to location NNN
+		//pc = 
+		break;
+
+	//0x2
+	case 0x2000: //2NNN - Call Subroutine at NNN
+
+		break;
+
+	//0x3
+	case 0x3000: //3XNN - Skip Next Instruction if (Vx == NN)
+
+		break;
+
+	//0x4
+	case 0x4000: //4XNN - Skip Next Instruction if (Vx != NN)
+
+		break;
+
+	//0x5
+	case 0x5000: //5XY0 - Skip Next Instruction if (Vx == Vy)
+
+		break;
+
+	//0x6
+	case 0x6000: //6XNN - Set Vx Register to NN
+
+		break;
+
+	//0x7
+	case 0x7000: //7XNN - Adds NN to Vx Then Stores Result in Vx (Vx += NN)
+
+		break;
+
+	//0x8
+	case 0x8000: //First 4 bits not enough so need to compare last 4 bits
+		switch (opcode & 0x000F)
+		{
+		case 0x0000: //8XY0 - Set Vx Register to Vy (Vx = Vy)
+			
+			break;
+		case 0x0001: //8XY1 - Bitwise OR (Vx = (Vx | Vy))
+
+			break;
+		case 0x0002: //8XY2 - Bitwise AND (Vx = (Vx & Vy))
+
+			break;
+		case 0x0003: //8XY3 - Bitwise XOR (Vx = (Vx ^ Vy))
+
+			break;
+		case 0x0004: //8XY4 - Add Vx to Vy and store in Vx (Vx += Vy). If result greater then 255 VF carry flag needs to be set
+
+			break;
+		case 0x0005: //8XY5 - Subtract Vy from Vx and store in Vx (Vx -= Vy). If Vx > Vy set VF to 1
+
+			break;
+		case 0x0006: //8XY6 - If the least significant bit of Vx is 1, then VF is set to 1. Then Vx is divided by 2.
+
+			break;
+		case 0x0007: //8XY7 - Subtract Vx from Vy and store in Vx (Vx = Vy - Vx). If Vy > Vx set VF to 1
+
+			break;
+		case 0x000E: //8XYE - If the most significant bit of Vx is 1, then VF is set to 1. Then Vx is multiplied by 2.
+
+			break;
+
+		default:
+			Log::logW("Unknown opcode: " + convertOpcodeToPrintableHex(opcode));
+			break;
+		}
+		break;
+
+	//0x9
+	case 0x9000: //9XY0 - Skip Next Instruction if (Vx != Vy)
+
+		break;
+
+	//0xA
 	case 0xA000: //ANNN - Sets I to the memory address NNN.
 		I = opcode & 0x0FFF;
 		pc += 2;
 		break;
 
+	//0xB
+	case 0xB000: //BNNN - Jump to location NNN + V0
+
+		break;
+
+	//0xC
+	case 0xC000: //CXNN - Generate Random Number Between 0-255, then AND with NN and store in Vx. (Vx = (rand(0-255) & NN))
+
+		break;
+
+	//0xD
+	case 0xD000: //DXYN - Draw sprite of N bytes starting at I in memory at pos (Vx, Vy). Set VF for collision event.
+
+		break;
+
+	//0xE
+	case 0xE000: //First 4 bits not enough so need to compare last 8 bits
+		switch (opcode & 0x00FF)
+		{
+		case 0x009E: //EX9E - Skip Next Instruction if key[Vx] is Pressed
+
+			break;
+		case 0x00A1: //EXA1 - Skip Next Instruction if key[Vx] is not Pressed
+
+			break;
+
+		default:
+			Log::logW("Unknown opcode: " + convertOpcodeToPrintableHex(opcode));
+			break;
+		}
+		break;
+
+	//0xF
+	case 0xF000: //First 4 bits not enough so need to compare last 8 bits
+		switch (opcode & 0x00FF)
+		{
+		case 0x0007: //FX07 - Set Vx to delay timer value
+			
+			break;
+		case 0x000A: //FX0A - Wait for key press, value of key stored in Vx. This Blocks All Execution Until Key Press.
+
+			break;
+		case 0x0015: //FX15 - Set Delay Timer to Vx
+
+			break;
+		case 0x0018: //FX18 - Set Sound Timer to Vx
+
+			break;
+		case 0x001E: //FX1E - Add Vx to I and store result in I
+
+			break;
+		case 0x0029: //FX29 - Set I to the memory location of sprite for character stored in Vx
+
+			break;
+		case 0x0033: //FX33 - Store the Binary-Coded Decimal representation of Vx in memory at locations I, I+1, I+2
+
+			break;
+		case 0x0055: //FX55 - Dump values from registry (V0 - Vx) in memory at address I and onwards. 'I' should not be modified
+
+			break;
+		case 0x0065: //FX065 - Load values from registry (V0 - Vx) in memory at address I and onwards. 'I' should not be modified
+
+			break;
+
+		default:
+			Log::logW("Unknown opcode: " + convertOpcodeToPrintableHex(opcode));
+			break;
+		}
+		break;
 	default:
 		Log::logW("Unknown opcode: " + convertOpcodeToPrintableHex(opcode));
 		break;
@@ -130,7 +279,7 @@ bool Chip8::loadROM(std::string path)
 
 	if (programRaw == nullptr)
 	{
-		fclose(programRaw);
+		//fclose(programRaw);
 
 		Log::logE("Could not load requested ROM, likely a invalid path. Provided Path: " + path);
 		return false;
@@ -152,7 +301,7 @@ bool Chip8::loadROM(std::string path)
 	{
 		Log::logE("Could not allocate memory to load ROM");
 		fclose(programRaw);
-		free(buffer);
+		//free(buffer);
 		return false;
 	}
 
